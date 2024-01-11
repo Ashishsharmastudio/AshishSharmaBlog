0635b231da73f02a5a954c90412c0e42f833bcd5
ashish sharma <ashishsharmastudio@gmail.com>
2024-01-11 18:54:29 +0530

    changes in slug  file

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typing";
 import { GetStaticProps } from "next";
-import Link from 'next/link';
+// import Link from 'next/link';
 // import { PortableText } from "@portabletext/react";
 import PortableText from "react-portable-text";
 import { useForm, SubmitHandler } from "react-hook-form";
-import comment from "../../personalblog/schemas/comment";
-import post from "../../personalblog/schemas/post";
+// import comment from "../../personalblog/schemas/comment";
+// import post from "../../personalblog/schemas/post";
 import { useSession } from "next-auth/react";
 
 
