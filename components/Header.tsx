58b1a474eb6c11223c8dac8adfefc4aef9a6d12c
ashish sharma <ashishsharmastudio@gmail.com>
2024-01-14 18:32:56 +0530

    sign in code commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,32 +36,35 @@ const Header = () => {
 
 
         </div>
-        {/* user login */}
-        <div className="flex items-center ml-auto gap-8 text-lg">
+                                 {/* user login */}
+                                 <div className="flex items-center gap-8 text-lg">
           <div className="flex items-center gap-1">
             <img
               className="w-8 h-8 rounded-full"
-              src={session?.user?.image ? session.user.image : '../public/man.png'}
+              src={
+                session?.user?.image 
+                  ? session.user.image
+                  : "https://www.noormohammad.live/static/media/roundedProfile.477a194221d255c8ce26.png"
+              }
+              alt="logo"
             />
             <p className="text-sm font-semibold text-slate-600	">
-              {session ? session?.user!.name : "Hello Stranger"}
-            </p>
+            {session?.user?.name || 'Hello Stranger'} 
+              </p>
           </div>
           {
-            session ? (<button onClick={() => signOut()}
-              className="uppercase text-sm text-slate-600 border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600">
-              Sign Out
+            session ? ( <button onClick={()=> signOut()} 
+            className="uppercase text-xs text-slate-600 border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600">
+            Sign Out
             </button>
-            ) : (
-              <button onClick={() => signIn()}
-                className="uppercase text-sm text-slate-600 border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600">
-                Sign In
-              </button>
+            ):(
+            <button onClick={()=> signIn()} 
+            className="uppercase text-xs text-slate-600 border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600">
+            Sign In
+          </button>
             )
-          }
-
-        </div>
-
+          }       
+        </div> 
         {/* Mobile menu button */}
         <button
           className="block md:hidden"
