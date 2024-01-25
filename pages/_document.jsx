a6339753cc669cec5875eebd09d6513f7e4628ac
ashish sharma <ashishsharmastudio@gmail.com>
2024-01-25 15:45:51 +0530

    MADE CHANGES IN ENV FILE

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
-    <Html Lang="en">
+    <Html lang="en">
       <Head>
+        {/* GTM head snippet */}
+        <script 
+        async
+        src={'https://www.googletagmanager.com/gtag/js?id=${GTM_ID}'}
+        />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GTM_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+          />
         {/* Google Search Console */}
         <meta
          name="google-site-verification"
@@ -17,6 +36,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* GTM body snippet */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MPC7B725');` }} />
       </body>
     </Html>
   );
