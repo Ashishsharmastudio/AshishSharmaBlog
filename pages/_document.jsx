573e2cd7414e22be46059668059ba00c4a2184c9
ashish sharma <ashishsharmastudio@gmail.com>
2024-02-03 12:32:23 +0530

    changed favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+// import ogImage from '../public/opengraph-image.png';
 
 export default function Document() {
   let GTM_ID = process.env.NEXT_PUBLIC_GTMhost_ID;
@@ -6,6 +7,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      {/* open graph tag */}
+      <meta property="og:title" content="Tech Blog Service - Honest Unbiased Tech Product Reviews" />
+          <meta property="og:description" content="Tech Blog Service provides detailed, honest, and unbiased reviews of 
+           the latest tech products in India and globally. We deeply research and test phones, laptops, TVs, audio devices,
+           smart home devices and more to provide comprehensive analysis and comparisons. Read our in-depth tech product 
+           reviews and determine the best gadgets in various price ranges and categories before buying. Follow Tech Blog 
+           Service to stay updated on new product launches and make smart purchasing decisions that get you the most 
+           value" />
+          <meta property="og:type" content="Blog Website"/>
+          <meta property="og:url" content="https://ashishsharmablog.tech/" />
+          <meta property="og:image" content="/public/opengraph-image.png" />  
         {/* GTM head snippet */}
         <script 
         async
