7220da459f0b81c970c8166b95836680a2ecc5c2
ashish sharma <ashishsharmastudio@gmail.com>
2024-02-03 09:33:43 +0530

    Your commit message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-
+import favicon from '../public/favicon.ico';
+import Head from 'next/head';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from 'next-auth/react'
@@ -6,6 +7,9 @@ import {SessionProvider} from 'next-auth/react'
 function MyApp({ Component, pageProps:{session,...pageProps} }: AppProps) {
   return (
   <SessionProvider session={session}>
+    <Head>
+        <link rel="icon" href="/favicon.ico" /> 
+    </Head>
       <Component {...pageProps} />
   </SessionProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home({ posts }: Props) {
               </Link>  
              ))
           }
-        </div>
+        </div>        
         {/* ================================================== */}
 
 
