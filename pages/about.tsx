33b4c7abe2fdc9d30fbcb5a0d5e45fc1aa2f900d
ashish sharma <ashishsharmastudio@gmail.com>
2024-02-06 07:15:54 +0530

    changed about page photo

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,7 +19,7 @@ const About = () => {
           <div className="flex flex-col md:flex-row">
             <div className="w-full md:w-1/2 mb-8 md:mb-0">
               <img
-                src="/ashu.JPG"
+                src="/man.png"
                 alt="MR.Ashish Sharma"
                 className="rounded-md shadow-md w-full h-auto"
               />
